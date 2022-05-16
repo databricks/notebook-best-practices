61093f861fc278478a8aa5288d77c91069dd1c49
michaelp-db <michael.piatek@databricks.com>
2022-05-16 13:41:52 -0700

    SP configuration basics.

--- terraform/databricks.tf
+++ terraform/databricks.tf
@@ -1,5 +1,6 @@
 provider "databricks" {
-  profile = "demo"
+  host = var.databricks_host
+  token = var.databricks_token
 }
 
 resource "databricks_service_principal" "testrunner" {
@@ -17,4 +18,18 @@ resource "databricks_permissions" "token_usage" {
 resource "databricks_token" "testrunner_pat" {
   comment  = "Test runner SP PAT"
   lifetime_seconds = 0
+}
+
+provider "databricks" {
+  alias = "sp"
+  host = var.databricks_host
+  token = databricks_token.testrunner_pat.token_value
+}
+
+resource "databricks_git_credential" "github" {
+  provider = databricks.sp
+
+  git_username = var.github_username
+  git_provider = "github"
+  personal_access_token = var.github_token
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,32 @@
+variable "databricks_host" {
+  description = "The host of the Databricks Workspace. Example: https://demo.cloud.databricks.com/"
+  type        = string
+}
+
+variable "databricks_token" {
+  description = "The PAT used to authenticate to the Databricks Workspace specified by databricks_host."
+  type        = string
+  sensitive   = true
+}
+
 variable "github_owner" {
-    description = "The owner of the GitHub repostiory."
-    type = string
+  description = "The owner of the GitHub repostiory. Example: Databricks"
+  type        = string
+}
+
+# TODO: Add a warning about SP GitHub permissions.
+variable "github_username" {
+  description = "Your GitHub username."
+  type        = string
 }
 
 variable "github_repository_name" {
-    description = "The name of the GitHub repostiory."
-    type = string
+  description = "The name of the GitHub repostiory. Example: notebook-best-practices"
+  type        = string
 }
 
 variable "github_token" {
-    description = "A GitHub authentication token."
-    type = string
-    sensitive = true
+  description = "A GitHub authentication token."
+  type        = string
+  sensitive   = true
 }
