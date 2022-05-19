9afd3fdff08d800edab5d943ec45e865ddc07a80
michaelp-db <michael.piatek@databricks.com>
2022-05-18 17:51:43 -0700

    Consolidate notebooks into a single directory to avoid dbx clobbers (#14)
    * Consolidate notebooks into a single directory to avoid dbx clobbers
    
    * Tweak paths
    
    Co-authored-by: michaelp-db <michaelp-db>

--- notebooks/run_unit_tests.py
+++ notebooks/run_unit_tests.py
@@ -9,9 +9,8 @@
 
 import pytest
 
+%cd ../tests
 retcode = pytest.main(["--junitxml=/tmp/test-unit.xml", "-lr", "."])
 
 # Fail the cell execution if we have any test failures.
 assert retcode == 0, 'The pytest invocation failed. See the log above for details.'
-
-# COMMAND ----------
