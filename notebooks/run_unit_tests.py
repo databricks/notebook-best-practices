50efb6adbb9e6280cb5d7d25b980d9f0acead628
michaelp-db <michael.piatek@databricks.com>
2022-05-19 17:06:56 -0700

    Distinguish test and normal notebook execution (#16)

--- notebooks/run_unit_tests.py
+++ notebooks/run_unit_tests.py
@@ -8,9 +8,24 @@
 # COMMAND ----------
 
 import pytest
+import os
 
-%cd ../tests
-retcode = pytest.main(["--junitxml=/tmp/test-unit.xml", "-lr", "."])
+# Run all tests in the repository root.
+notebook_path = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+repo_root = os.path.dirname(os.path.dirname(notebook_path))
+os.chdir(f'/Workspace/{repo_root}')
+%pwd
+
+# pytest.main runs our tests directly in the notebook environment, providing
+# fidelity for Spark and other configuration variables.
+#
+# A limitation of this approach is that changes to the test will be
+# cache by Python's import caching mechanism.
+#
+# To iterate on tests during development, use 'Detach & Re-attach'
+# between invocations in order to restart the Python process and
+# thus clear the import cache to pick up changes.
+retcode = pytest.main(["."])
 
 # Fail the cell execution if we have any test failures.
 assert retcode == 0, 'The pytest invocation failed. See the log above for details.'
